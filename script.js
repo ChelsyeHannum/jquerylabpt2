017588862da089fa99bfa6eb082d8725412584bf
chelsyehannum <chelsye.hannum@gmail.com>
2018-07-31 22:33:08 -0400

    adjust X img

--- script.js
+++ script.js
@@ -13,15 +13,10 @@ $(() => {
         $("#seatform").fadeIn("slow");
     });
 
-
-
     $("body").on("click", "#table div.available", (e) => {
         $("#seatform").show();
         $("#seatform #seatforminfo form p").text(`Table Number: ${$(e.target).text()}`);
         currentTable = $(e.target);
-
-
-
 
     });
 
@@ -31,6 +26,9 @@ $(() => {
         $("#button").click(function() {
             $("#seatform").fadeOut("slow")
         });
+        $("img").click(function() {
+            $("#seatform").fadeOut("slow")
+        });
         if (e.target.tagName === "BUTTON") {
             currentTable
                 .removeClass("available")
@@ -38,9 +36,9 @@ $(() => {
                 .data("guest-name", $("input").eq(0).val())
                 .data("guest-phone", $("input").eq(1).val())
                 .data("guest-size", $("input").eq(2).val());
-            console.dir(currentTable.data("guest-name"));
-            console.dir(currentTable.data("guest-phone"));
-            console.dir(currentTable.data("guest-size"));
+            // console.dir(currentTable.data("guest-name"));
+            // console.dir(currentTable.data("guest-phone"));
+            // console.dir(currentTable.data("guest-size"));
         }
     });
 
@@ -50,10 +48,5 @@ $(() => {
         console.log($(e.target));
         $("p.guest-name").text(`Name: ${$(e.target).data("guest-name")}`);
         $("p.guest-size").text(`Size of party: ${$(e.target).data("guest-size")}`);
-
-
-
-
     });
-
 });
